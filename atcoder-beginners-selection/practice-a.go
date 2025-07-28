f3f86df177c8d1b24226cd31787633316ffa2b6c
kainoika <kainokai0131@gmail.com>
2025-07-29 03:38:38 +0900

    add: AtCoder Beginners SelectionのPracticeA, ABC086Aの回答を作成

---
+++ atcoder-beginners-selection/practice-a.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int
+	fmt.Scanf("%d", &a)
+
+	var b, c int
+	fmt.Scanf("%d %d", &b, &c)
+
+	var s string
+	fmt.Scanf("%s", &s)
+
+	fmt.Printf("%d %s\n", a+b+c, s)
+}
