8a0520bb5ed93772ed51d0302eb8e4470f19182e
kainoika <kainokai0131@gmail.com>
2025-08-03 18:16:09 +0900

    add: FizzBuzzプログラムを追加

---
+++ other/fizzbuzz.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	for i := 1; i <= n; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
